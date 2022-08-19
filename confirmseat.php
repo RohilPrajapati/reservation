dbe29eef26009d1f8c6b54d477a32c311eecaa0e
RohilPrajapati <rohilprajapati@gmail.com>
2022-08-19 20:50:06 +0545

    added validation of resevered seat

--- confirmseat.php
+++ confirmseat.php
@@ -12,14 +12,23 @@
         }
         $seat = mysqli_fetch_assoc($result);
         $seat_id = $seat['id'];
-        $q_insert_reserve = "INSERT INTO reserved_seat values(".$user_id.", ".$movie_id.", ".$seat_id.")";
-        echo $q_insert_reserve;
-        if(mysqli_query($con,$q_insert_reserve)){
-            echo "Seat reserved";
-            header("location:seat.php?movie_id=".$movie_id);
+        $q_check_reserved_seat = "SELECT * FROM reserved_seat where movie_id= ".$movie_id." and seat_id = ".$seat_id;
+        $result = mysqli_query($con,$q_check_reserved_seat);
+        // echo $q_check_reserved_seat;
+        // echo $con->error;
+        if(mysqli_num_rows($result)==0){
+            $q_insert_reserve = "INSERT INTO reserved_seat values(".$user_id.", ".$movie_id.", ".$seat_id.")";
+            echo $q_insert_reserve;
+            if(mysqli_query($con,$q_insert_reserve)){
+                echo "Seat reserved";
+                header("location:seat.php?movie_id=".$movie_id);
+            }else{
+                echo "fail to reserved";
+                header("location:seat.php?movie_id=".$movie_id);
+            }
         }else{
-            echo "fail to reserved";
-            header("location:seat.php?movie_id=".$movie_id);
+            echo "Seat has been already reserved please try other seat.";
+            die();
         }
 
     }
